37bf345112bde7d65bc5a5de689ec20bb9d25787
Dmtry-Miroshkin <dmm@mail.ru>
2019-12-20 14:25:57 +0300

    ДЗ сделано

--- src/HomeWork/Lesson2.java
+++ src/HomeWork/Lesson2.java
@@ -6,31 +6,51 @@ import java.util.Random;
 public class Lesson2 {
     public static void main(String[] args) {
         Random rand = new Random();
-        MyArr arr = new MyArr(1000); // создаем обьект типа MyArr с массивом
-        for (int i = 0; i < 1000; i++) {// заполняем массив произвольными числами
-            arr.arr[i] = (rand.nextInt(1000));
+        MyArr arr = new MyArr(1000000); // создаем обьект типа MyArr с массивом
+        for (int i = 0; i < 1000000; i++) {// заполняем массив произвольными числами
+            arr.arr[i] = (rand.nextInt(1000000));
             arr.size++;
         }
-        arr.display(); //вывод полученного массива
+//        arr.display(); //вывод полученного массива
         int search = 45; // поиск в массиве
 
-        System.out.println(arr.find(search));
-//        arr.sortBubble();
-        arr.sortSelect();
-        arr.display();
-        System.out.println(arr.binaryFind(566));
+        int[] cloneArr = new int[arr.size];
+        System.arraycopy(arr.arr, 0, cloneArr, 0, arr.size);
+        System.out.println("Запуск пузырьковой сортировки!");
+        long startTime = System.currentTimeMillis();
+        arr.sortBubble(cloneArr);
+        long timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("Пузырьковый сорт занял " + timeSpent + " миллисекунд");
+
+        int[] cloneArr2 = new int[arr.size];
+        System.arraycopy(arr.arr, 0, cloneArr2, 0, arr.size);
+        System.out.println("Запуск сортировки методом выбора!");
+        startTime = System.currentTimeMillis();
+        arr.sortSelect(cloneArr2);
+        timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("Сортировка методом выбора заняла " + timeSpent + " миллисекунд");
+
+        int[] cloneArr3 = new int[arr.size];
+        System.arraycopy(arr.arr, 0, cloneArr3, 0, arr.size);
+        System.out.println("Запуск сортировки методом вставки!");
+        startTime = System.currentTimeMillis();
+        arr.sortInsert(cloneArr3);
+        timeSpent = System.currentTimeMillis() - startTime;
+        System.out.println("Сортировка методом вставки заняла " + timeSpent + " миллисекунд");
+//        arr.display(cloneArr3);
+        System.out.println("Есть ли в массиве число 566? " + arr.binaryFind(cloneArr3, 566));
     }
 
     static class MyArr {
         int[] arr;
         int size;
 
-        public MyArr(int capacity) {
+        MyArr(int capacity) {
             this.size = 0;
             arr = new int[capacity];
         }
 
-        public boolean binaryFind(int value) { // двоичный поиск
+        boolean binaryFind(int[] arr, int value) { // двоичный поиск
             int low = 0;
             int high = this.size - 1;
             int mid;
@@ -59,8 +79,8 @@ public class Lesson2 {
             return false;
         }
 
-        public void display() { // вывод массива в консоль
-            for (int i = 0; i < this.size; i++) {
+        public void display(int[] arr) { // вывод массива в консоль
+            for (int i = 0; i < size; i++) {
                 System.out.println(arr[i]);
             }
         }
@@ -89,48 +109,51 @@ public class Lesson2 {
             this.size++;
         }
 
-        public void sortBubble() { // Пузырьковая сортировка
+        void sortBubble(int[] ar) { // Пузырьковая сортировка
             int out, in;
-            for (out = this.size - 1; out >= 1; out--) {
+            for (out = size - 1; out >= 1; out--) {
                 for (in = 0; in < out; in++) {
-                    if (this.arr[in] > this.arr[in + 1]) {
-                        change(in, in + 1);
+                    if (ar[in] > ar[in + 1]) {
+                        change(in, in + 1, ar);
                     }
                 }
             }
         }
 
-        private void change(int a, int b) {
-            int tmp = this.arr[a];
-            this.arr[a] = this.arr[b];
-            this.arr[b] = tmp;
+        private void change(int a, int b, int[] ar2) {
+            int tmp = ar2[a];
+            ar2[a] = ar2[b];
+            ar2[b] = tmp;
         }
 
-        public void sortSelect() { //Сортировка методом выбора
+        void sortSelect(int[] ar3) { //Сортировка методом выбора
             int out, in, mark;
             for (out = 0; out < this.size; out++) {
                 mark = out;
                 for (in = out + 1; in < this.size; in++) {
-                    if (this.arr[in] < this.arr[mark]) {
+                    if (ar3[in] < ar3[mark]) {
                         mark = in;
                     }
                 }
-                change(out, mark);
+                change(out, mark, ar3);
             }
         }
 
-        public void sortInsert(){ //Сортировка методом вставки
+        void sortInsert(int[] arr) { //Сортировка методом вставки
             int in, out;
-            for(out = 1;out < this.size; out++){
-                int temp = this.arr[out];
+            for (out = 1; out < this.size; out++) {
+                int temp = arr[out];
                 in = out;
-                while(in > 0 && this.arr[in-1] >=temp){
-                    this.arr[in] = this.arr[in-1];
+                while (in > 0 && arr[in - 1] >= temp) {
+                    arr[in] = arr[in - 1];
                     --in;
                 }
-                this.arr[in] = temp;
+                arr[in] = temp;
             }
         }
+//        Пузырьковый сорт занял 1974518 миллисекунд
+//        Сортировка методом выбора заняла 798743 миллисекунд
+//        Сортировка методом вставки заняла 140264 миллисекунд
 
     }
 }
