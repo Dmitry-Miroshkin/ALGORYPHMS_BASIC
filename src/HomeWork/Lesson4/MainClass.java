40684c05f8463b1b2f301540ba7e2c000a8b985e
Dmitry-Miroshkin <52827207+Dmitry-Miroshkin@users.noreply.github.com>
2019-12-26 22:15:45 +0300

    initial

---
+++ src/HomeWork/Lesson4/MainClass.java
@@ -0,0 +1,7 @@
+package HomeWork.Lesson4;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/HomeWork/Lesson4/MyLinkedList.java
@@ -0,0 +1,4 @@
+package HomeWork.Lesson4;
+
+public class MyLinkedList {
+}
