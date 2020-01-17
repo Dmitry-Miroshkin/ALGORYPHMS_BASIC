42f4ddfc28bb36643385ea2a14aad1252b834a04
Dmtry-Miroshkin <dmm@mail.ru>
2020-01-17 15:46:24 +0300

    showItems немного поправил

--- src/HomeWork/Lesson5/BackpackTask.java
+++ src/HomeWork/Lesson5/BackpackTask.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class BackpackTask {
 
 
-     static ArrayList<Item> items;
+    static ArrayList<Item> items;
 
     static void AddItems() {
         ArrayList<Item> items = new ArrayList<>();
@@ -17,11 +17,14 @@ public class BackpackTask {
         items.add(new Item("Котелок", 1, 500));
     }
 
-    private static void ShowItems(ArrayList<Item> _items) {
+    private static String ShowItems(ArrayList<Item> _items) {
         StringBuilder sb = new StringBuilder("[ ");
         for (Item i : _items) {
-            sb.append(i.name + Double.toString(i.weigth) + Double.toString(i.price));
+            sb.append(i.name + ", " + Double.toString(i.weigth) + ", " + Double.toString(i.price));
         }
+        sb.delete(sb.length() - 2, sb.length());
+        sb.append(" ]");
+        return sb.toString();
     }
 
     private static void Solve() {
