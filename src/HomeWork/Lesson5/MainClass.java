b417a5fecee20fec6f350fce478944b301e461b6
Dmtry-Miroshkin <dmm@mail.ru>
2020-01-17 14:30:52 +0300

    возведение числа в степень рекурсией сделано

--- src/HomeWork/Lesson5/MainClass.java
+++ src/HomeWork/Lesson5/MainClass.java
@@ -1,7 +1,27 @@
 package HomeWork.Lesson5;
 
+import java.util.Scanner;
+
 public class MainClass {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int a = sc.nextInt();
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Введите степень: ");
+        int b = sc1.nextInt();
+        System.out.println(a + " в степени " + b + " равно " + f(a, b));
+    }
 
+    public static double f(double base, int ex) {
+        if (ex > 0) {
+            if (ex % 2 == 1) { // Нечетное ?
+                return base * f(base, ex - 1);
+            } else {
+                double res = f(base, ex / 2);
+                return res * res;
+            }
+        } else
+            return 1;
     }
 }
