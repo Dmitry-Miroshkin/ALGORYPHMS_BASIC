a9093aad9c52e4c34f6377b084f5f95898009941
Dmitry-Miroshkin <52827207+Dmitry-Miroshkin@users.noreply.github.com>
2020-01-26 22:15:07 +0300

    ДЗ сделано

--- src/HomeWork/Lesson8/Main.java
+++ src/HomeWork/Lesson8/Main.java
@@ -1,4 +1,17 @@
 package HomeWork.Lesson8;
 
 public class Main {
+    public static void main(String[] args) {
+        ChainingHashMap<Integer, String> map = new ChainingHashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        map.put(5, "five");
+        map.delete(3);
+        System.out.println(map.get(3));
+        System.out.println(map);
+        map.put(3, "three");
+        System.out.println(map.get(3));
+    }
 }
